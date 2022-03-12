d79dad98428b8e77aeb90f3e10d8175e47d18553
riskigalang <riskigalangsaputra@gmail.com>
2022-03-12 11:53:27 +0700

    setup project

---
+++ src/main/java/com/riskigalang/implementjwt/ImplementJwtApplication.java
@@ -0,0 +1,13 @@
+package com.riskigalang.implementjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riskigalang/implementjwt/ImplementJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riskigalang.implementjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
